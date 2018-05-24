365e785c11a44265ee15319e48cd2ff6bc0a618f
zmazk123 <39406652+zmazk123@users.noreply.github.com>
2018-05-24 13:05:01 +0200

    Add files via upload

---
+++ podatkovna baza/Testni podatki.sql
@@ -0,0 +1,19 @@
+use MojObrtnik;
+
+insert into Obrtnik values(null,'Miha','Gapiha','31245','miha@gmail.com','123-345-756',null,'Kolko ja radim majkemi...', 'mihč','123');
+
+insert into Naslov values(null, 'Hrastnik', 3450, 'Partizanska', 12,1);
+
+insert into Kategorija values(null, 'Mizarstvo');
+
+insert into Storitev values(null, 'Miha s.p.', 'svašta', '2015-01-01', '2015-01-02',1,1);
+
+insert into Stanje_narocila values(null, 'Potrjeno');
+
+insert into Uporabnik values(null, 'Lojze', 'Hojze', 'lojze@gmail.com', null, '756-123-544', 'lojz', '532');
+
+insert into Narocilo values(null, 213, '2015-01-01', 'comentar', '756-123-544', '2015-01-02', '2015-10-02', 1,1,1);
+
+insert into Ocena values(null, 5,1,1);
+
+insert into Komentar values(null, 'ta bulš',1,1);
